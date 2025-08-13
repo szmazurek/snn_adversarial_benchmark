b2962f878faeb685ebc2cd12af05d06b91dded26
Szymon Mazurek <szymonmazurek57@gmail.com>
2025-08-13 21:19:36 +0200

    Recurrent nets

--- src/models.py
+++ src/models.py
@@ -2,6 +2,7 @@ from torch import nn
 from spikingjelly.activation_based import surrogate, neuron, layer, encoding
 from spikingjelly.activation_based.model.sew_resnet import sew_resnet18
 from spikingjelly.activation_based.model.spiking_vgg import spiking_vgg11_bn
+from spikingjelly.activation_based.layer import LinearRecurrentContainer
 from typing import Dict, Callable, Any
 
 
@@ -100,6 +101,35 @@ def SimpleConvSNN(
     return net
 
 
+def SimpleConvSNNRecurrent(
+    n_channels: int = 1,
+    output_size: int = 10,
+    neuron_model: neuron.BaseNode = neuron.LIFNode,
+    surrogate_function: surrogate.SurrogateFunctionBase = surrogate.ATan,
+) -> nn.Module:
+    net = nn.Sequential(
+        layer.Conv2d(n_channels, 64, kernel_size=3, padding=1, bias=False),
+        layer.BatchNorm2d(64),
+        neuron_model(surrogate_function=surrogate_function()),
+        layer.MaxPool2d(kernel_size=2, stride=2),
+        layer.Conv2d(64, 128, kernel_size=3, padding=1, bias=False),
+        layer.BatchNorm2d(128),
+        neuron_model(surrogate_function=surrogate_function()),
+        layer.MaxPool2d(kernel_size=2, stride=2),
+        layer.AdaptiveAvgPool2d((1, 1)),
+        layer.Flatten(),
+        layer.Linear(128, 64),
+        layer.BatchNorm1d(64),
+        LinearRecurrentContainer(
+            neuron_model(surrogate_function=surrogate_function()),
+            in_features=64,
+            out_features=64,
+        ),
+        layer.Linear(64, output_size),
+    )
+    return net
+
+
 def _simple_MLP_SNN(
     n_channels: int = 784,
     output_size: int = 10,
@@ -116,6 +146,39 @@ def _simple_MLP_SNN(
         layer.Linear(256, 64),
         layer.BatchNorm1d(64),
         neuron_model(surrogate_function=surrogate_function()),
+        layer.Linear(64, output_size),
+    )
+    return net
+
+
+def _simple_MLP_SNN_recurrent(
+    n_channels: int = 784,
+    output_size: int = 10,
+    neuron_model: neuron.BaseNode = neuron.LIFNode,
+    surrogate_function: surrogate.SurrogateFunctionBase = surrogate.ATan,
+) -> nn.Module:
+    net = nn.Sequential(
+        layer.Linear(n_channels, 512),
+        layer.BatchNorm1d(512),
+        LinearRecurrentContainer(
+            neuron_model(surrogate_function=surrogate_function()),
+            in_features=512,
+            out_features=512,
+        ),
+        layer.Linear(512, 256),
+        layer.BatchNorm1d(256),
+        LinearRecurrentContainer(
+            neuron_model(surrogate_function=surrogate_function()),
+            in_features=256,
+            out_features=256,
+        ),
+        layer.Linear(256, 64),
+        layer.BatchNorm1d(64),
+        LinearRecurrentContainer(
+            neuron_model(surrogate_function=surrogate_function()),
+            in_features=64,
+            out_features=64,
+        ),
         layer.Linear(64, output_size),
     )
     return net
@@ -150,4 +213,5 @@ MODEL_MAP: Dict[str, Callable[[Any], nn.Module]] = {
     "spiking_vgg": SpikingVGG11BN,
     "simple_conv_snn": SimpleConvSNN,
     "simple_mlp_snn": SimpleMLPSNN,
+    "simple_conv_snn_recurrent": SimpleConvSNNRecurrent,
 }
